4f3fd04ae0260bda0ac2738f1d5732b44cdeed1e
muhammaddariazzidane <dariazzidane@gmail.com>
2023-11-27 20:52:13 +0700

    fix(detail-page): update Breadcrumbs

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -2,7 +2,7 @@ import { ChevronRight } from 'lucide-react';
 
 export default function Breadcrumbs() {
   return (
-    <div className="w-full text-left">
+    <div className="w-full my-5 text-left">
       <nav aria-label="breadcrumb" className="block w-full">
         <ul className="flex w-full gap-4 lg:space-x-5 flex-wrap items-center text-sm py-4 px-4">
           <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
@@ -13,19 +13,13 @@ export default function Breadcrumbs() {
           </li>
           <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
             <a href="#">
-              <span>Catalog</span>
+              <span>Produk</span>
             </a>
             <ChevronRight size={20} />
           </li>
           <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
             <a href="#">
               <span>Smartphone</span>
-            </a>
-            <ChevronRight size={20} />
-          </li>
-          <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
-            <a href="#">
-              <span>Apple</span>
             </a>
             <ChevronRight size={20} />
           </li>
