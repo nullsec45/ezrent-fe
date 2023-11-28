d19de466e86b9b43b0e66358c6808f9fb6542d3e
muhammaddariazzidane <dariazzidane@gmail.com>
2023-11-28 13:28:53 +0700

    feat(dashboard-address): update profile page

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,14 +1,12 @@
 'use client';
 
-import Breadcrumbs from '@/components/Breadcrumbs';
-import { Button } from '../ui/button';
-import { FileClock, FileText, History, LogOut, User2 } from 'lucide-react';
-import Link from 'next/link';
-import { PiMapPinFill } from 'react-icons/pi';
-import Image from 'next/image';
-import UpdateProfileForm from '../elements/form/UpdateProfileForm';
-import { useState } from 'react';
+import UpdateProfileForm from '@/components/elements/form/UpdateProfileForm';
+import SmallMenu from '@/components/elements/menu/SmallMenu';
 import { useForm, Controller } from 'react-hook-form';
+import Breadcrumbs from '@/components/Breadcrumbs';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function Profile() {
   const [nameImage, setNameImage] = useState('');
@@ -25,78 +23,7 @@ export default function Profile() {
       <Breadcrumbs mainTitle={'Profil Saya'} />
       <div className="flex lg:flex-nowrap flex-wrap gap-4 my-12 lg:px-3">
         {/* menu */}
-        <div className="rounded-xl max-w-xs w-full border-[1.4px] h-fit shadow p-3">
-          <ul className="flex flex-col gap-1 ">
-            <li>
-              <Link
-                href="/"
-                className="py-1 w-full hover:bg-gray-200 hover:translate-x-2 inline-block transition-all duration-300 rounded-lg"
-              >
-                <Button className="hover:bg-transparent bg-transparent text-black flex gap-2 items-center">
-                  <User2 />
-                  Profil Saya
-                </Button>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="py-1 w-full hover:bg-gray-200 hover:translate-x-2 inline-block transition-all duration-300 rounded-lg"
-              >
-                <Button className="hover:bg-transparent bg-transparent text-black flex gap-2 items-center">
-                  <PiMapPinFill size={25} />
-                  Daftar Alamat
-                </Button>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="py-1 w-full hover:bg-gray-200 hover:translate-x-2 inline-block transition-all duration-300 rounded-lg"
-              >
-                <Button className="hover:bg-transparent bg-transparent text-black flex gap-2 items-center">
-                  <FileText />
-                  Kelengkapan Data
-                </Button>
-              </Link>
-            </li>
-          </ul>
-          <hr className="w-full h-[1.5px] bg-gray-400 my-3" />
-          <ul className="flex flex-col gap-1 ">
-            <li>
-              <Link
-                href="/"
-                className="py-1 w-full hover:bg-gray-200 hover:translate-x-2 inline-block transition-all duration-300 rounded-lg"
-              >
-                <Button className="hover:bg-transparent bg-transparent text-black flex gap-2 items-center">
-                  <FileClock />
-                  Menunggu Pembayaran
-                </Button>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="py-1 w-full hover:bg-gray-200 hover:translate-x-2 inline-block transition-all duration-300 rounded-lg"
-              >
-                <Button className="hover:bg-transparent bg-transparent text-black flex gap-2 items-center">
-                  <History />
-                  Daftar Transaksi
-                </Button>
-              </Link>
-            </li>
-          </ul>
-          <hr className="w-full h-[1.5px] bg-gray-400 my-3" />
-          <Link
-            href="/"
-            className="py-1 w-full hover:bg-gray-200 hover:translate-x-2 inline-block transition-all duration-300 rounded-lg"
-          >
-            <Button className="hover:bg-transparent bg-transparent text-red-500  flex gap-2 items-center">
-              <LogOut />
-              Logout
-            </Button>
-          </Link>
-        </div>
+        <SmallMenu />
         {/* menu */}
         {/* update profile */}
         <div className="rounded-xl p-3 w-full border shadow">
