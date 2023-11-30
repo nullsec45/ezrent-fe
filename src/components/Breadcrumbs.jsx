3b62d46ef9292f03e11bb90e293e6aee137f7bb0
fltmzl <fltmzl2810@gmail.com>
2023-11-30 18:34:10 +0700

    fix(detail-product-api): implement Backend API on the Detail Product Page

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,42 +1,38 @@
 'use client';
 
 import { ChevronRight } from 'lucide-react';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+// import { usePathname } from 'next/navigation';
 
-export default function Breadcrumbs({ mainTitle }) {
-  const path = usePathname();
-  const firstPath = path.split('/')?.[1];
-  const secondPath = path.split('/')?.[2];
+export default function Breadcrumbs({ items }) {
+  console.log(items);
+  // const path = usePathname();
+  // const firstPath = path.split('/')?.[1];
+  // const secondPath = path.split('/')?.[2];
 
   return (
     <div className="w-full my-5 text-left">
       <nav aria-label="breadcrumb" className="block w-full">
-        <ul className="flex w-full gap-4 lg:space-x-5 flex-wrap items-center capitalize text-sm py-4 px-4">
+        <ul className="flex w-full lg:space-x-5 flex-wrap items-center capitalize text-sm py-4 px-4">
           <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
-            <a href="#">
+            <Link href="#">
               <span>Beranda</span>
-            </a>
+            </Link>
             <ChevronRight size={20} />
           </li>
-          <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
-            <a href="#">
-              <span>{firstPath}</span>
-            </a>
-            <ChevronRight size={20} />
-          </li>
-          {secondPath && (
-            <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
-              <a href="#">
-                <span>{secondPath}</span>
-              </a>
-              <ChevronRight size={20} />
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className="flex cursor-pointer space-x-3 items-center text-gray-500"
+            >
+              <Link href={item.link}>
+                <span>{item.name}</span>
+              </Link>
+
+              {/* jangan tampilkan icon chevron ketika sudah di array terakhir */}
+              {index !== items.length - 1 && <ChevronRight size={20} />}
             </li>
-          )}
-          <li className="flex cursor-pointer space-x-3 items-center  text-black font-semibold">
-            <a href="#">
-              <span>{mainTitle ? mainTitle : 'iPhone 15 Promag'}</span>
-            </a>
-          </li>
+          ))}
         </ul>
       </nav>
     </div>
---
+++ src/components/elements/detailProduct/ProductDescription.jsx
@@ -0,0 +1,15 @@
+import useDetailProduct from '@/hooks/api/useDetailProduct';
+import ProductDescriptionSkeleton from '@/components/elements/skeleton/ProductDescriptionSkeleton';
+
+export default function ProductDescription({ productId }) {
+  const { data: product, isLoading } = useDetailProduct(productId);
+
+  if (isLoading) return <ProductDescriptionSkeleton />;
+
+  return (
+    <div className="p-7 shadow-md rounded-xl mb-12">
+      <h1 className="text-xl font-medium">Details</h1>
+      <p className="my-6 text-sm text-gray-500">{product.description}</p>
+    </div>
+  );
+}
