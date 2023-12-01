44734be6640df9e616801bca26306448b9a35b37
muhammaddariazzidane <dariazzidane@gmail.com>
2023-12-01 18:57:42 +0700

    fix(navbar): fix position top

--- src/components/elements/navbar/NavbarMenuMobile.jsx
+++ src/components/elements/navbar/NavbarMenuMobile.jsx
@@ -1,20 +1,3 @@
-import { useState } from 'react';
-import Link from 'next/link';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
-import {
-  CopyCheck,
-  History,
-  LogOutIcon,
-  MapPin,
-  MenuIcon,
-  Plus,
-  Search,
-  ShoppingCart,
-  User,
-  X,
-} from 'lucide-react';
-import OpenStoreIcon from '@/components/icons/OpenStoreIcon';
 import NavbarMenuMobileAuthenticated from './NavbarMenuMobileAuthenticated';
 import useUser from '@/hooks/api/useUser';
 import NavbarMenuMobileNotAuthenticated from './NavbarMenuMobileNotAuthenticated';
@@ -26,7 +9,7 @@ export default function NavbarMenuMobile({ isMenuOpen }) {
     <div
       className={`${
         isMenuOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0 '
-      } origin-top transition-all duration-500 ease-in-out md:hidden absolute top-[4.2rem] z-50 bg-none inset-0 min-h-screen  text-black`}
+      } origin-top transition-all duration-500 ease-in-out md:hidden absolute top-[4.2rem] sm:top-[3.6rem] z-50 bg-none inset-0 min-h-screen  text-black`}
     >
       {user ? (
         <NavbarMenuMobileAuthenticated user={user} />
