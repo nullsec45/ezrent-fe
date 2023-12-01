bc4619b59557511a7331f07eb5df479cb3eed22f
muhammaddariazzidane <dariazzidane@gmail.com>
2023-12-01 08:33:26 +0700

    fix(alert): update breadcrumbs

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -2,35 +2,35 @@
 
 import { ChevronRight } from 'lucide-react';
 import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
 
 export default function Breadcrumbs({ items }) {
-  console.log(items);
-  // const path = usePathname();
-  // const firstPath = path.split('/')?.[1];
-  // const secondPath = path.split('/')?.[2];
-
   return (
     <div className="w-full my-5 text-left">
       <nav aria-label="breadcrumb" className="block w-full">
         <ul className="flex w-full lg:space-x-5 flex-wrap items-center capitalize text-sm py-4 px-4">
           <li className="flex cursor-pointer space-x-3 items-center  text-gray-500">
-            <Link href="#">
+            <Link href="/">
               <span>Beranda</span>
             </Link>
             <ChevronRight size={20} />
           </li>
-          {items.map((item, index) => (
+          {items?.map((item, index) => (
             <li
               key={index}
-              className="flex cursor-pointer space-x-3 items-center text-gray-500"
+              className={twMerge(
+                'flex cursor-pointer space-x-3 items-center',
+                index === items?.length - 1
+                  ? 'text-black font-medium'
+                  : 'text-gray-500'
+              )}
             >
               <Link href={item.link}>
                 <span>{item.name}</span>
               </Link>
 
               {/* jangan tampilkan icon chevron ketika sudah di array terakhir */}
-              {index !== items.length - 1 && <ChevronRight size={20} />}
+              {index !== items?.length - 1 && <ChevronRight size={20} />}
             </li>
           ))}
         </ul>
