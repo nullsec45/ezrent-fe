05c7f6cba345819877d2882f642750dd7c471f55
fltmzl <fltmzl2810@gmail.com>
2023-12-06 19:57:05 +0700

    feat(open-store): delete lat & lng validation in storeSchema

--- src/config/constant/store/initialStoreValues.js
+++ src/config/constant/store/initialStoreValues.js
@@ -9,6 +9,4 @@ export const initialStore = {
   subDistrict: '',
   fullAddress: '',
   postalCode: '',
-  latitude: '',
-  longitude: '',
 };
--- src/config/schema/store/storeSchema.js
+++ src/config/schema/store/storeSchema.js
@@ -3,16 +3,14 @@ import * as yup from 'yup';
 export const storeSchema = yup
   .object({
     name: yup.string().required(),
+    phoneNumber: yup.string().required().min(10).max(15),
     description: yup.string().required(),
     accountNumber: yup.string().required(),
-    phoneNumber: yup.string().required(),
     province: yup.string().required(),
     city: yup.string().required(),
     district: yup.string().required(),
     subDistrict: yup.string().required(),
     fullAddress: yup.string().required(),
     postalCode: yup.string().required(),
-    latitude: yup.string().required(),
-    longitude: yup.string().required(),
   })
   .required();
