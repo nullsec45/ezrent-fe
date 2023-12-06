218df8b7c74d76e8482b72f7bf0368422276a603
muhammaddariazzidane <dariazzidane@gmail.com>
2023-12-06 16:02:22 +0700

    feat(open-store): update open store form

--- src/components/elements/form/OpenStoreForm.jsx
+++ src/components/elements/form/OpenStoreForm.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { Controller, useForm } from 'react-hook-form';
+import ErrorMessageInput from '../errors/ErrorMessageInput';
 import {
   Card,
   CardContent,
@@ -8,17 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { initialStore } from '@/config/constant/store/initialStoreValues';
-import { storeSchema } from '@/config/schema/store/storeSchema';
-import { yupResolver } from '@hookform/resolvers/yup';
-import dynamic from 'next/dynamic';
-import { useMemo, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import FieldInput from '@/components/elements/input/FieldInput';
-import ErrorMessageInput from '@/components/elements/errors/ErrorMessageInput';
 import ButtonSubmit from '../button/ButtonSubmit';
-import { Label } from '@/components/ui/label';
-import { Textarea } from '@/components/ui/textarea';
 import {
   Select,
   SelectContent,
@@ -27,45 +18,109 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
+import FieldInput from '../input/FieldInput';
+import { Textarea } from '@/components/ui/textarea';
 import useProvinces from '@/hooks/api/useProvinces';
+import { useEffect, useMemo, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { useEdgeStore } from '@/lib/edgestore';
+import { toast } from '@/components/ui/use-toast';
+import { addStore } from '@/utils/api';
 import useCities from '@/hooks/api/useCities';
 import useDistricts from '@/hooks/api/useDistricts';
 import useSubDistricts from '@/hooks/api/useSubDistricts';
 import usePostalCode from '@/hooks/api/usePostalCode';
+import useSWR from 'swr';
+import axios from 'axios';
+
+const fetcher = async (url) => {
+  const res = await axios.get(url);
+  return res.data;
+};
 
 export default function OpenStoreForm() {
+  const { edgestore } = useEdgeStore();
+
   const Map = useMemo(
     () =>
       dynamic(() => import('@/components/elements/map/Map'), { ssr: false }),
     []
   );
+
   const {
     register,
     handleSubmit,
     control,
     reset,
     formState: { errors, isSubmitting },
-  } = useForm({
-    defaultValues: initialStore,
-    resolver: yupResolver(storeSchema),
-  });
-
-  // position ini isinya LATITUDE & LONGITUDE
-  const [position, setPosition] = useState(null);
-  console.log(position);
+  } = useForm();
+  const [provinceId, setProvinceId] = useState(null);
+  const [cityId, setCityId] = useState(null);
+  const [districtId, setDistrictId] = useState(null);
 
   const { data: provinces } = useProvinces();
-  // const { data: cities } = useCities(provinceId); // ini GET Kota berdasarkan ID Provinsi nya
-  // const { data: districts } = useDistricts(cityId); // ini GET Kecamatan berdasarkan ID Kota nya
-  // const { data: subDistricts } = useSubDistricts(districtId); // ini GET Kelurahan berdasarkan ID Kecataman nya
-  // const { data: postalCode } = usePostalCode(cityId, districtId); // ini GET KodePos berdasarkan ID Kota && ID Kecamatan nya
+
+  const [position, setPosition] = useState(null);
+  // {last, lng} <- posisi
+
+  const { data: cities } = useCities(provinceId); // ini GET Kota berdasarkan ID Provinsi nya
+  const { data: districts } = useDistricts(cityId); // ini GET Kecamatan berdasarkan ID Kota nya
+  const { data: subDistricts } = useSubDistricts(districtId); // ini GET Kelurahan berdasarkan ID Kecataman nya
+  const { data: postalCodes } = usePostalCode(cityId, districtId); // ini GET KodePos berdasarkan ID Kota && ID Kecamatan nya
 
   const handleOpenStore = async (data) => {
-    console.log('SUBMIT');
-  };
+    console.log(data);
+    const profilePicture = data?.profilePicture?.[0];
 
+    if (data.profilePicture.length !== 0) {
+      try {
+        const res = await edgestore.publicFiles.upload({
+          file: profilePicture,
+        });
+
+        const store = {
+          name: data?.name,
+          profilePicture: res?.url,
+          description: data?.description,
+          accountNumber: data?.accountNumber,
+          storeAddress: {
+            province: data?.province,
+            city: data?.city,
+            district: data?.district,
+            subDistrict: data?.subDistrict,
+            fullAddress: data?.fullAddress,
+            postalCode: position?.postalCode,
+            latitude: position?.lat,
+            longitude: position?.lng,
+          },
+        };
+
+        const response = await addStore(store);
+        if (response.status === 201) {
+          reset(initialStore);
+          toast({
+            title: 'Success',
+            description: response.data?.message,
+          });
+        }
+      } catch (error) {
+        toast({
+          variant: 'destructive',
+          title: 'Uh oh! Something went wrong.',
+          description: error?.message,
+        });
+      }
+    } else {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        description: 'Please pick one picture',
+      });
+    }
+  };
   return (
-    <div>
+    <>
       <Card>
         <form onSubmit={handleSubmit(handleOpenStore)}>
           <CardHeader>
@@ -124,24 +179,30 @@ export default function OpenStoreForm() {
               <ErrorMessageInput message={errors.profilePicture?.message} />
             </div>
 
-            <div className="flex flex-col space-y-1.5">
+            <div className="flex flex-col space-y-1.5 ">
               <Label htmlFor="province">Provinsi</Label>
               <Controller
                 control={control}
-                id="province"
                 name="province"
                 render={({ field }) => (
                   <Select
-                    onValueChange={field.onChange}
+                    onValueChange={(value) => {
+                      const province = value.split(',');
+                      field.onChange(province[1]);
+                      setProvinceId(province[0]);
+                    }}
                     defaultValue={field.value}
                   >
                     <SelectTrigger>
                       <SelectValue placeholder="Pilih Provinsi" />
                     </SelectTrigger>
-                    <SelectContent>
+                    <SelectContent className="z-[9999]">
                       <SelectGroup>
                         {provinces?.map((province) => (
-                          <SelectItem value={province.text} key={province.id}>
+                          <SelectItem
+                            value={`${province.id},${province.text}`}
+                            key={province.id}
+                          >
                             {province.text}
                           </SelectItem>
                         ))}
@@ -152,17 +213,149 @@ export default function OpenStoreForm() {
               />
               <ErrorMessageInput message={errors.province?.message} />
             </div>
+            <div className="flex flex-col space-y-1.5 ">
+              <Label htmlFor="city">Kota</Label>
+              <Controller
+                control={control}
+                name="city"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={(value) => {
+                      const city = value.split(',');
+                      field.onChange(city[1]);
+                      setCityId(city[0]);
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pilih kota" />
+                    </SelectTrigger>
+                    <SelectContent className="z-[9999]">
+                      <SelectGroup>
+                        {cities?.map((city) => (
+                          <SelectItem
+                            value={`${city.id},${city.text}`}
+                            key={city.id}
+                          >
+                            {city.text}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <ErrorMessageInput message={errors.city?.message} />
+            </div>
+            <div className="flex flex-col space-y-1.5 ">
+              <Label htmlFor="district">Kecamatan</Label>
+              <Controller
+                control={control}
+                name="district"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={(value) => {
+                      const district = value.split(',');
+                      field.onChange(district[1]);
+                      setDistrictId(district[0]);
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pilih Kecamatan" />
+                    </SelectTrigger>
+                    <SelectContent className="z-[9999]">
+                      <SelectGroup>
+                        {districts?.map((district) => (
+                          <SelectItem
+                            value={`${district.id},${district.text}`}
+                            key={district.id}
+                          >
+                            {district.text}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <ErrorMessageInput message={errors.district?.message} />
+            </div>
+            <div className="flex flex-col space-y-1.5 ">
+              <Label htmlFor="subDistrict">Kelurahan</Label>
+              <Controller
+                control={control}
+                name="subDistrict"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={(value) => {
+                      const subDistrict = value.split(',');
+                      field.onChange(subDistrict[1]);
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pilih Kelurahan" />
+                    </SelectTrigger>
+                    <SelectContent className="z-[9999]">
+                      <SelectGroup>
+                        {subDistricts?.map((subDistrict) => (
+                          <SelectItem
+                            value={`${subDistrict.id},${subDistrict.text}`}
+                            key={subDistrict.id}
+                          >
+                            {subDistrict.text}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <ErrorMessageInput message={errors.subDistrict?.message} />
+            </div>
+            <div className="flex flex-col space-y-1.5 ">
+              <Label htmlFor="postalCode">Kode POS</Label>
+              <Controller
+                control={control}
+                name="postalCode"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={(value) => {
+                      const postalCode = value.split(',');
+                      field.onChange(postalCode[1]);
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pilih Kelurahan" />
+                    </SelectTrigger>
+                    <SelectContent className="z-[9999]">
+                      <SelectGroup>
+                        {postalCodes?.map((postalCode) => (
+                          <SelectItem
+                            value={`${postalCode.id},${postalCode.text}`}
+                            key={postalCode.id}
+                          >
+                            {postalCode.text}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <ErrorMessageInput message={errors.subDistrict?.message} />
+            </div>
+            <div className="flex flex-col space-y-1.5 ">
+              <Map position={position} setPosition={setPosition} />
+            </div>
           </CardContent>
-
-          <div className="px-7">
-            <Map position={position} setPosition={setPosition} />
-          </div>
-
           <CardFooter>
             <ButtonSubmit isSubmitting={isSubmitting} text="Buka Toko" />
           </CardFooter>
         </form>
       </Card>
-    </div>
+    </>
   );
 }
